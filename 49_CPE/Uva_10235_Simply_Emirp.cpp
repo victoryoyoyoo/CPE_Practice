e2ca0335025563f7944400b90d508e803bbaf139
yoyoo <houvictor941216@gmail.com>
2026-08-06 16:42:25 +0800

    Update Uva_10235_Simply_Emirp.cpp

--- 49_CPE/Uva_10235_Simply_Emirp.cpp
+++ 49_CPE/Uva_10235_Simply_Emirp.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 bool isPrime(int N){
-	if(N <= 1) return false;
-    for(int i = 2; i * i <= N;++i) if(N % i == 0) return false;
+    if(N <= 1) return false;
+    for(int i = 2; i * i <= N; ++i) if(N % i == 0) return false;
     return true;
 }
 
