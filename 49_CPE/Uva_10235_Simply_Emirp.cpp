0fb068425100edfddaacf303f778e0af5c95b359
yoyoo <houvictor941216@gmail.com>
2026-08-06 16:34:34 +0800

    Fix formatting and syntax issues in Simply Emirp program

--- 49_CPE/Uva_10235_Simply_Emirp.cpp
+++ 49_CPE/Uva_10235_Simply_Emirp.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 bool isPrime(int N){
-    if(N <= 1) return false;
+	if(N <= 1) return false;
     for(int i = 2; i * i <= N;++i) if(N % i == 0) return false;
     return true;
 }
