8b15473d71886dec21f651e955c0e12c8e4506c5
yoyoo <houvictor941216@gmail.com>
2026-08-09 17:30:04 +0800

    Add files via upload

---
+++ 49_CPE/Uva_10420_List_of_Conquests.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    if(cin >> n){
+        map<string, int> record;
+        while(n--){
+            string country, name;
+            cin >> country;
+            getline(cin, name);
+            
+            ++record[country];
+        }
+        for(auto x : record) cout << x.first << " " << x.second << "\n";
+    }
+    return 0;
+}
